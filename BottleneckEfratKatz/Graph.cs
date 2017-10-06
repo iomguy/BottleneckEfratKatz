92ea858c9eb1a10cb894756c288109f11ae0adb2
Alexander Kalyuzhnyuk <iom96@inbox.ru>
2017-10-06 18:28:58 +0300

    Edges are added to class Graph

--- BottleneckEfratKatz/Graph.cs
+++ BottleneckEfratKatz/Graph.cs
@@ -11,13 +11,16 @@ namespace BottleneckEfratKatz
             ///
             ArcsList = new List<Arc>(); //тут стоит использовать массив
             DistI    = new List<double>();
+            EdgesDict = new Dictionary<Dot, HashSet<Dot>>();
         }
 
         private List<Arc> _arcsList;
         private List<double> _distI;
+        private Dictionary<Dot, HashSet<Dot>> _edges;
 
         public List<Arc> ArcsList { get => _arcsList; set => _arcsList = value; }
         public List<double> DistI { get => _distI;    set => _distI    = value; }
+        public Dictionary<Dot, HashSet<Dot>> EdgesDict { get => _edges; set => _edges = value; }
 
         ///тут хранятся расстояния в графе G по возрастанию и без повторений
 
@@ -34,6 +37,12 @@ namespace BottleneckEfratKatz
                         Arc arcAB = new Arc(dotA, dotB);
 
                         ArcsList.Add(arcAB);
+
+                        if (EdgesDict.ContainsKey(dotA))
+                            EdgesDict[dotA].Add(dotB);
+                        else
+                            EdgesDict[dotA] = new HashSet<Dot> { dotB };
+
                         if (!(DistI.Contains(arcAB.Distance))) ///добавляем расстояние, если такого ещё не было
                             DistI.Add(arcAB.Distance);
                     }                    
